9106845e2b613757f236a571465eb9f71f7d013b
matmath <mathieu.k.legault@gmail.com>
2017-11-16 11:31:12 +0000

    Splitting into multiple part and adding scale

--- src/app/graph/smallstep/smallstep.component.ts
+++ src/app/graph/smallstep/smallstep.component.ts
@@ -4,14 +4,15 @@ import { Component, ElementRef, Input, ViewChild, ViewEncapsulation, OnInit } fr
 
 @Component({
   selector: 'small-step',
-  template: '<div class="smallstep" #smallstep></div>',
+  template: '<svg class="smallstep" #smallstep></svg>',
   styleUrls: ['./smallstep.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class SmallStepComponent implements OnInit {
   @ViewChild('smallstep') private chartContainer: ElementRef;
-  @Input() private data: Array<any>;
-
+  @Input() private data: number[];
+  tableWidth:number = 800;
+  barHeight:number = 20;
   ngOnInit() {
     if (this.data) {
       this.createChart();
@@ -19,13 +20,30 @@ export class SmallStepComponent implements OnInit {
   }
 
   createChart() {
+    const { data, barHeight, tableWidth } = this;
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([0, this.tableWidth]); // Add max window / container / media width.
+
     const element = this.chartContainer.nativeElement;
-    d3.select(element)
-    .selectAll("div")
-    .data(this.data)
-      .enter()
-      .append("div")
-      .style("width", (d) => (d + "px"))
-      .text((d) => d);
+    const chart = d3.select(".smallstep")
+        .attr("width", tableWidth)
+        .attr("height", barHeight * data.length);
+
+    const bar = chart.selectAll("g")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", x)
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) { return (x(d) - 3); })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d; });
   }
 }
