9a5a048920a9688326c6b23eb1b5bbcba8d63cd2
Mathieu <mathieu.k.legault@gmail.com>
2018-03-11 20:10:01 +0100

    Adding test validation to make sure we do not overwrite defaults

--- src/app/company/company.component.spec.ts
+++ src/app/company/company.component.spec.ts
@@ -16,4 +16,30 @@ describe('Company.component', () => {
     expect(ctrl).toBeDefined();
     expect(ctrl.listView).toBeDefined();
   });
+
+  it('initial variables', () => {
+    expect(ctrl.emptyCie).toBeDefined();
+    expect(ctrl.showList).toBe(true);
+    expect(ctrl.showCie).toBe(false);
+    expect(ctrl.showRecruters).toBe(false);
+    expect(ctrl.submitting).toBe(false);
+  });
+
+  it('#listView', () => {
+    ctrl.showList = false; // before click (on by default)
+    ctrl.listView();
+    expect(ctrl.showList).toBe(true, 'Visible after click');
+  });
+
+  it('#newCie', () => {
+    ctrl.newCie(true);
+    expect(ctrl.showCie).toBe(true);
+    expect(ctrl.activeCie).toEqual(ctrl.emptyCie, 'reset the Cie');
+    expect(ctrl.activeRecruters).toEqual(ctrl.emptyRecruters, 'reset the Recruters');
+    // Validate that we do not modify the initial Value
+    ctrl.activeCie.location = 'Montreal';
+    ctrl.activeRecruters.name = 'Marvel';
+    expect(ctrl.activeCie).not.toEqual(ctrl.emptyCie, 'Default Cie');
+    expect(ctrl.activeRecruters).not.toEqual(ctrl.emptyRecruters, 'Default Recruters');
+  })
 });
