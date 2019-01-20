b72eadd5ae24913822aa0a6a6a8f583971b6462f
Mathieu <mathieu.k.legault@gmail.com>
2019-01-20 18:21:27 -0500

    fix: 🐛 fontawesome
    Fix the new FA type of reference

--- src/app/generic/sortOrder.component.ts
+++ src/app/generic/sortOrder.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'sorting-order',
   template: `
-    <i class="fa fa-arrows-v" *ngIf="orderby !== comparable"></i>
-    <i class="fa fa-arrow-down" *ngIf="orderby === comparable && order"></i>
-    <i class="fa fa-arrow-up" *ngIf="orderby === comparable && !order"></i>
+    <i class="fas fa-arrows-alt-v" *ngIf="orderby !== comparable"></i>
+    <i class="fas fa-arrow-down" *ngIf="orderby === comparable && order"></i>
+    <i class="fas fa-arrow-up" *ngIf="orderby === comparable && !order"></i>
   `,
 })
 export class SortingOrder {
